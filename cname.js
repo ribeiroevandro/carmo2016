8d946bcac420cc81d96f9f6c56acf6b38d3b2808
Evandro Ribeiro <ribeiroevandro@live.com>
2016-08-25 22:52:12 -0300

    alter cname.js

--- cname.js
+++ cname.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 
 try {
   fs.accessSync('./build', fs.F_OK);
-  fs.writeFileSync('./build/CNAME', 'ribeiroevandro.com.br/carmo2016')
+  fs.writeFileSync('./build/CNAME', 'carmo2016.ribeiroevandro.com.br')
   console.log('SUCCESS: CNAME file was created!')
 } catch (e) {
   console.log('ERROR: Missing build folder!')
